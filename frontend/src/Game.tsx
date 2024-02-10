a1026aec83d8c89dbb071a0738c942e5daaf4c56
Liza George <liza@chiramattel.com>
2024-02-09 21:09:51 -0600

    Add basic styling to Boards

--- frontend/src/Game.tsx
+++ frontend/src/Game.tsx
@@ -2,9 +2,17 @@ import Board from "./components/board";
 
 const Game = () => {
   return (
-    <div className="flex flex-row ">
-      <Board question="Percent of groovy people in CT" picture_id="CT" />
-      <Board question="Percent of funky people in AL" picture_id="AL" />
+    <div className="flex flex-row h-screen">
+      <Board
+        className="w-1/2 bg-slate-500"
+        question="Percent of groovy people in CT"
+        picture_id="CT"
+      />
+      <Board
+        className="w-1/2 bg-teal-500"
+        question="Percent of funky people in AL"
+        picture_id="AL"
+      />
     </div>
   );
 };
--- frontend/src/components/board.tsx
+++ frontend/src/components/board.tsx
@@ -3,14 +3,15 @@ import React from "react";
 interface BoardProps {
   question: string;
   picture_id: string;
+  className?: string;
 }
 
-const Board: React.FC<BoardProps> = ({ question, picture_id }) => {
+const Board: React.FC<BoardProps> = ({ question, picture_id, className }) => {
   return (
-    <div>
-      <h2>{question}</h2>
+    <div className={`${className} flex flex-col h-full items-center justify-center`}>
+      <h2 className="w-fit text-xl">{question}</h2>
       {/* TODO: retrieve picture from db based on picture id */}
-      <p>{picture_id}</p>
+      {/* <p>{picture_id}</p> */}
     </div>
   );
 };
