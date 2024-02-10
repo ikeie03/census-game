900b92b6a86da2f8941f52c9a456154690ee0c81
Liza George <liza@chiramattel.com>
2024-02-09 20:53:52 -0600

    Quick tailwind example

--- frontend/src/Game.tsx
+++ frontend/src/Game.tsx
@@ -1,12 +1,12 @@
 import Board from "./components/board";
 
 const Game = () => {
-    return (
-      <div>
-        <Board question="Percent of groovy people in CT" picture_id="CT" />
-        <Board question="Percent of funky people in AL" picture_id="AL" />
-      </div>
-    );
-}
- 
+  return (
+    <div className="flex flex-row ">
+      <Board question="Percent of groovy people in CT" picture_id="CT" />
+      <Board question="Percent of funky people in AL" picture_id="AL" />
+    </div>
+  );
+};
+
 export default Game;
