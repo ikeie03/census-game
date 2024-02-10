4a0620efe358b31ed16619e814b8d1b43be042f4
Liza George <liza@chiramattel.com>
2024-02-09 20:13:29 -0600

    Install tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="bg-slate-300 md:bg-red-500">
+      <p className="text-2xl font-bold underline">test</p>
+    </div>
+  );
 }
 
 export default App
