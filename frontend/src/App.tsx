6beb2dd0e24304d70b6e30a0cec864523e4e9ac7
Isaac Exposito <fortiscadere03@gmail.com>
2024-02-09 20:04:16 -0600

    implemented react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import Home from './Home.tsx'
+import Game from './Game.tsx'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/game" element={<Game/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
