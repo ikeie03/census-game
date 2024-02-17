e01616a869bc1013e123dda941781033044fa9c4
Liza George <liza@chiramattel.com>
2024-02-16 19:35:22 -0600

    Style score texts

--- frontend/src/components/score_display.tsx
+++ frontend/src/components/score_display.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface ScoreDisplayProps {
   score: number;
@@ -13,14 +13,16 @@ const ScoreDisplay: React.FC<ScoreDisplayProps> = ({
 }) => {
   return (
     <div
-      className={`${className} w-fit flex flex-col items-center justify-center text-white`}
+      className={`${className} w-fit flex flex-col items-center justify-center`}
       style={{ top: "calc(50% + 30px)" }}
     >
-      <div className="bg-white w-fit px-6 py-8 rounded-full text-black ">
+      <div className="bg-white w-fit px-6 py-8 rounded-full text-black mb-2">
         <p className="text-5xl">OR</p>
       </div>
-      <p>Score: {score}</p>
-      <p>High Score: {highScore}</p>
+      <div className=" text-white text-lg drop-shadow-[-1.6px_2px_0.9px_rgba(0,0,0,1)] text-center">
+        <p>Score: {score}</p>
+        <p>High Score: {highScore}</p>
+      </div>
     </div>
   );
 };
