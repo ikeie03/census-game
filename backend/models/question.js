e57c09d7aa14b5ba30af26e84cd9ccae9fceedad
Isaac Exposito <fortiscadere03@gmail.com>
2024-02-16 18:32:43 -0600

    sanity push

--- backend/models/question.js
+++ backend/models/question.js
@@ -5,6 +5,10 @@ const QuestionSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  state: {
+    type: String,
+    required: true,
+  },
   answer: {
     type: Number,
     required: true,
---
+++ backend/routes/game.js
@@ -0,0 +1,35 @@
+// NOTE: it's erc
+
+const express = require("express");
+const router = express.Router();
+
+const Question = require("../models/question.js");
+const Streak = require("../models/streak.js");
+
+// @route   GET api/questions/:number
+// @desc    Get a new question from specified id number
+// @access  Public
+router.get("/questions/:number", (req, res) => {
+  console.log("sanity check");
+  console.log(req.params.number);
+  Question.findOne({ number: req.params.number })
+    .then((question) => {
+      if (question) {
+        res.json(question);
+      } else {
+        res.status(404).json({ questionnotfound: "Question not found" });
+      }
+    })
+    .catch((err) =>
+      res.status(404).json({ questionnotfound: "Question not found" })
+    );
+});
+
+router.put("/streaks/:streak", (req, res) => {
+  const inserted_streak = new Streak({
+    length: req.params.streak,
+  });
+  inserted_streak.save();
+});
+
+module.exports = router;
