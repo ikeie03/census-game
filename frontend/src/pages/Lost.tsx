a36d85605a67771fb4aed3bf6f60c4dad161469e
Liza George <liza@chiramattel.com>
2024-09-30 17:08:06 -0500

    Link back to home page from lost page

--- frontend/src/pages/Lost.tsx
+++ frontend/src/pages/Lost.tsx
@@ -8,6 +8,9 @@ const Lost = () => {
   const handleTryAgain = () => {
     navigate("/game", { state: { score: 0, highScore: highScore } });
   };
+  const handleGoHome = () => {
+    navigate("/");
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-darkblue text-white text-center">
@@ -18,14 +21,21 @@ const Lost = () => {
 
       <div className="bg-teal text-white rounded-lg p-8 shadow-md">
         <p className="text-2xl">Your Score: {score}</p>
-        <p className="text-2xl mt-2">High Score: {highScore}</p>
+        <p className="text-2xl mt-2">Your high Score: {highScore}</p>
       </div>
 
       <button
         onClick={handleTryAgain}
-        className="mt-8 bg-darkred hover:bg-yellow text-white py-2 px-6 rounded-lg text-xl transition"
+        className="mt-8 bg-darkred hover:bg-yellow text-white py-4 px-12 rounded-lg text-3xl transition"
       >
         Try Again
+      </button>
+
+      <button
+        onClick={handleGoHome}
+        className="mt-8 bg-offwhite hover:bg-yellow text-teal py-2 px-6 rounded-lg text-xl transition"
+      >
+        Go back home
       </button>
     </div>
   );
